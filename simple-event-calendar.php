a283fe03fffc4aaca1b628efa63facbb02fcb1c2
elton-sudo <elton@nerdw.com>
2024-02-29 01:11:53 +0200

    Add pluging entry file.

---
+++ simple-event-calendar.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Simple Event Calendar
+Description: Simple event calendar plugin.
+Version: 1.0
+Author: Elton Brown
+*/
+
+require_once plugin_dir_path( __FILE__ ) . "init.php";
