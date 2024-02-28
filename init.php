31c86eeae2e6d67ef20266c9f92c2797a2b7e81c
elton-sudo <elton@nerdw.com>
2024-02-29 01:11:22 +0200

    Update init file.

--- init.php
+++ init.php
@@ -1,12 +1,6 @@
 <?php
 
-/*
-Plugin Name: Simple Event Calendar
-Description: Simple event calendar plugin.
-Version: 1.0
-Author: Elton Brown
-*/
-
 require_once plugin_dir_path( __DIR__ ) . "Src/Core/Controller.php";
 require_once plugin_dir_path( __DIR__ ) . "Src/Entity/Event.php";
 require_once plugin_dir_path( __DIR__ ) . "Src/Repo/EventRepository.php";
+require_once plugin_dir_path( __DIR__ ) . "Src/Contollers/DefaultController.php";
