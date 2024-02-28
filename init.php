1e12cea39281583c48307cf25857628706100e29
elton-sudo <elton@nerdw.com>
2024-02-28 23:12:26 +0200

    Create init file.

---
+++ init.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Simple Event Calendar
+Description: Simple event calendar plugin.
+Version: 1.0
+Author: Elton Brown
+*/
+
+require_once plugin_dir_path( __DIR__ ) . 'Core/Controller.php';
+require_once plugin_dir_path( __DIR__ ) . "Core/App.php";
