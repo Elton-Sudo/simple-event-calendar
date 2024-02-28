bf915836b56126ce980016697fb8be57b10b64d9
elton-sudo <elton@nerdw.com>
2024-02-29 00:11:56 +0200

    Create init file.

--- init.php
+++ init.php
@@ -7,5 +7,6 @@ Version: 1.0
 Author: Elton Brown
 */
 
-require_once plugin_dir_path( __DIR__ ) . 'Core/Controller.php';
-require_once plugin_dir_path( __DIR__ ) . "Core/App.php";
+require_once plugin_dir_path( __DIR__ ) . "Src/Core/Controller.php";
+require_once plugin_dir_path( __DIR__ ) . "Src/Entity/Event.php";
+require_once plugin_dir_path( __DIR__ ) . "Src/Repo/EventRepository.php";
