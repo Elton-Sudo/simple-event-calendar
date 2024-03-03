acf90095e3ac2d52ad8005386f8156b4282e975d
elton-sudo <elton@nerdw.com>
2024-03-03 13:35:24 +0200

    Add link.

--- simple-event-calendar.php
+++ simple-event-calendar.php
@@ -4,7 +4,7 @@
 Plugin Name: Simple Event Calendar
 Description: Simple event calendar plugin.
 Version: 1.0
-Author: Elton Brown
+Author: <a href='https://eltonbrown.co.za'>Elton Brown</a>
 */
 
 require_once plugin_dir_path( __FILE__ ) . "init.php";
