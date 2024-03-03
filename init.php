66222a27cad14fb4e414c33f8e920f1bfc1caddf
elton-sudo <elton@nerdw.com>
2024-03-03 14:08:04 +0200

    Update init.

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once plugin_dir_path( __DIR__ ) . "Src/Core/Controller.php";
-// require_once plugin_dir_path( __DIR__ ) . "Src/Entity/Event.php";
-// require_once plugin_dir_path( __DIR__ ) . "Src/Repo/EventRepository.php";
-// require_once plugin_dir_path( __DIR__ ) . "Src/Contollers/DefaultController.php";
+require_once plugin_dir_path(__FILE__) . "Src/Core/Controller.php";
+require_once plugin_dir_path(__FILE__) . "Src/Entity/Event.php";
+require_once plugin_dir_path(__FILE__) . "Src/Repo/EventRepository.php";
+require_once plugin_dir_path(__FILE__) . "Src/Controllers/DefaultController.php";
