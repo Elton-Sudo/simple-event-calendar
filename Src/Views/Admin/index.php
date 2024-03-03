bced46e4e7a7e52ac74d1cbe42972e7f2bf0d983
elton-sudo <elton@nerdw.com>
2024-03-03 21:04:14 +0200

    Update admin template.

--- Src/Views/Admin/index.php
+++ Src/Views/Admin/index.php
@@ -14,13 +14,25 @@
         </tr>
     </thead>
     <tbody>
-        <?php
 
-        
-        foreach ($events as $event) :
-            $event;
-        ?>
-        
+        <?php foreach ($events as $event) : ?>
+
+        <tr>
+            <td><?php echo esc_html($event->title); ?></td>
+            <td><?php echo esc_html($event->description); ?></td>
+            <td><?php echo esc_html($event->date); ?></td>
+            <td><?php echo esc_html($event->time); ?></td>
+            <td><?php echo esc_html($event->location); ?></td>
+            <td><?php echo esc_html($event->type); ?></td>
+            <td><?php echo esc_html(implode(', ', $event->keywords)); ?></td>
+            <td>
+                <a href="<?php echo esc_url(add_query_arg(['action' => 'edit_event', 'event_id' => $event->id])); ?>">Edit</a>
+                |
+                <a href="<?php echo esc_url(add_query_arg(['action' => 'delete_event', 'event_id' => $event->id])); ?>">Delete</a>
+            </td>
+        </tr>
+
         <?php endforeach; ?>
+
     </tbody>
 </table>
