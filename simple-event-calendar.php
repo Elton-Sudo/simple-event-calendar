10388c81ff91fea1e4849a36b3c028ce69514518
elton-sudo <elton@nerdw.com>
2024-03-03 20:09:33 +0200

    Update plugin entry file.

--- simple-event-calendar.php
+++ simple-event-calendar.php
@@ -7,7 +7,7 @@ Version: 1.0
 Author: <a href='https://eltonbrown.co.za'>Elton Brown</a>
 */
 
-require_once plugin_dir_path( __FILE__ ) . "installation.php";
-register_activation_hook(__FILE__, ['Installation', 'simple_event_calendar_install']);
-
 require_once plugin_dir_path( __FILE__ ) . "init.php";
+
+add_action('admin_menu', ['AdminController', 'register_menu']);
+register_activation_hook(__FILE__, ['Installation', 'simple_event_calendar_install']);
